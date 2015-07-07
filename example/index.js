411aacc9d0afa35be786ecaed7f6d40227966d53
vrunoa <vruno.alassia@gmail.com>
2015-07-07 10:19:49 -0300

    add example, run: npm run example

---
+++ example/index.js
@@ -0,0 +1,43 @@
+var ADB = new (require('../bin/adbjs'))();
+
+var version = ADB.version();
+console.log(version);
+
+// Getting devices
+var devices = ADB.devices();
+console.log(devices)
+
+var info = ADB.packageInfo("com.urucas.cineros");
+console.log(info);
+
+
+var isAvailable = ADB.isDeviceAvailable(devices[0].id);
+console.log(isAvailable);
+/*
+var packages = ADB.listPackages();
+console.log(packages);
+*/
+
+var flag = ADB.isPackageInstalled("com.urucas.zoster_testapp");
+console.log(flag);
+
+var flag = ADB.isAppRunning("com.urucas.zoster_testapp");
+console.log(flag);
+
+var flag = ADB.install("./zoster_testapp.apk", "com.urucas.zoster_testapp");
+console.log(flag);
+
+var flag = ADB.uninstall("com.urucas.zoster_testapp");
+console.log(flag);
+
+ADB.promptUninstall("com.urucas.cineros");
+
+var deviceInfo = ADB.deviceInfo(devices[0].id);
+console.log(deviceInfo);
+
+/*
+var processList = ADB.userProcessList(devices[0]);
+console.log(processList);
+*/
+
+
