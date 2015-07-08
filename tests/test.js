9a3b78df55b0ad81889c2550659d34e6eada18e1
vrunoa <vruno.alassia@gmail.com>
2015-07-08 15:49:51 -0300

    update test

--- tests/test.js
+++ tests/test.js
@@ -6,6 +6,7 @@ describe("ADB instance test", () => {
 
   it("Test adb version", (done) => {
     let version = adb.version();
+    console.log(version);
     if(!/Android Debug Bridge version [\d\.]+/.test(version)) throw new Error("returns wrong version:"+version);
     done();
   });
