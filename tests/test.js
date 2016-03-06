535bb98cc42a78b80081003b9f9041dfa28e546c
vrunoa <vruno.alassia@gmail.com>
2016-03-06 13:35:31 -0300

    separate monitor tests

--- tests/test.js
+++ tests/test.js
@@ -8,18 +8,6 @@ describe("adbjs tests", () => {
   
   let adb = new ADB();
 
-  it("should create an event Emitter for montor method", (done) => {
-    let devices = adb.devices()
-    let monitor = adb.monitor()
-    monitor.should.not.equal(null)
-    monitor.should.not.equal(undefined)
-    expect(monitor).to.be.a("object")
-    expect(monitor.info).to.be.a("function")
-    expect(monitor.info()).to.be.a("object")
-    expect(monitor.on).to.be.a("function")
-    done()
-  })
-  
   it("should return version", (done) => {
     let version = adb.version();
     if(!/Android Debug Bridge version [\d\.]+/.test(version)) 
